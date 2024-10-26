d422f8b1dab53387903255f00e13b67671bb5272
Douglas Engratulis <dengratulis@gmail.com>
2024-10-26 14:02:47 -0300

    Criado struct produtos...

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct { // Estrutura heterogenea de produtos... (Vinicius)
+    int id;
+    int qtd;
+    float precoUnidade;
+    float desconto;
+    float valorFinal;
+    char nomeProduto[100];
+} produto;
+
 int main()
 {
     printf("Hello world!\n");
