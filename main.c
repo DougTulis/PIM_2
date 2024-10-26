ea6a18fdff083badcee1b033f6f506775d79fc99
Douglas Engratulis <dengratulis@gmail.com>
2024-10-26 14:11:11 -0300

    Terminado a o submenu estoque e switch case...

--- main.c
+++ main.c
@@ -16,8 +16,34 @@ void removerItem();
 void listarItens();
 
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+ int opcao;
+    do {
+        printf("\n=== Menu de Estoque ===\n"); // submenu do estoque...
+        printf("1. Adicionar Item\n");
+        printf("2. Remover Item\n");
+        printf("3. Listar Itens\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+                // adicionarItem(); (em manutenção..)
+                break;
+            case 2:
+                // removerItem(); (em manutenção..)
+                break;
+            case 3:
+                listarItens();
+                break;
+            case 0:
+                printf("Saindo do programa...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while(opcao != 0);
+
     return 0;
 }
