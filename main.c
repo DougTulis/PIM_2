033788995cccd5b2d74acb8e9c71273ead65f419
Douglas Engratulis <dengratulis@gmail.com>
2024-10-27 11:06:57 -0300

    Testando laço para reconhecer itens de estoque ´já pertencente...

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main() {
 
 
 void listarItens() {
-      printf("\n=== Lista de Produtos ===\n");
+    printf("\n=== Lista de Produtos ===\n");
     for (int i = 0; i < totalProdutos; i++) {
         printf("ID: %d\n", produtos[i].id);
         printf("Nome: %s\n", produtos[i].nomeProduto);
@@ -93,49 +93,54 @@ void carregarProdutos() {
     }
     fclose(arquivo);
     /// Teste com arquivo-estoque que eu criei....
-    FILE *arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\ProdutoEstoque.txt", "r");
+    int idEstoque, idQtd;
+    FILE *arquivoEstoque = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\ProdutoEstoque.txt", "r");
     if (arquivo == NULL) {
         printf("Arquivo não existe."); // programação defensiva!
         exit(1);
-}
+    }
+    while(fscanf(arquivoEstoque, "ID do produto: %d%*[^\n]\n%*[^\n]\n%*[^\n]\n%*[^\n]\nEstoque: %d\n", &idEstoque, &idQtd) == 2) { // aqui vai ler o id (primeira linha), vai pular 4 linhas e depois ler o estoque...
 
-void salvarProdutosVinculadoComEstoque() {
-    FILE *arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\ProdutoEstoque.txt", "w"); // PASTA TXT NOVA QUE VOU ESCREVER OS PRODUTOS COM ESTOQUE
-    if (arquivo == NULL) {
-        printf("Arquivo não existe."); // programação defensiva...
-        exit(1);
     }
 
-    for (int i = 0; i <totalProdutos; i++ ) { // aqui sera um laço for pra tipo percorrer todos os structs de produtos e assim escreve-los na nova txt
-        fprintf(arquivo, "ID do produto: %d\n Produto: %s\n Preço por unidade: %.2f\n Desconto aplicado: %.2f\n Valor Final: %.2f\n Estoque: %d\n",produtos[i].id,
-                produtos[i].nomeProduto,
-                produtos[i].precoUnidade,
-                produtos[i].desconto,
-                produtos[i].valorFinal,
-                produtos[i].qtd);
-    }
-    fclose(arquivo);
-}
-
-void adicionarItemEstoque() {
-    carregarProdutos();
-    bool idExiste = false; // boolean pra controlar o laço se o id existir no array struct.
-    int id, quantidade; // declarando o id e quantidade que do produto em questao...
-    printf("Insira o ID do produto: ");
-    scanf("%d", &id);
-    for (int i = 0; i <totalProdutos; i++) {
-        if (produtos[i].id == id) {
-            idExiste = true;
-            printf("Produto selecionado: %s\n", produtos[i].nomeProduto);
-            printf("Digite a quantidade de estoque do produto: " );
-            scanf("%d", &quantidade);
-            produtos[i].qtd += quantidade;
-            printf("Estoque atualizado com sucesso!\n Estoque atual do produto: %d", produtos[i].qtd);
-            break;
+    void salvarProdutosVinculadoComEstoque() {
+        FILE *arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\ProdutoEstoque.txt", "w"); // PASTA TXT NOVA QUE VOU ESCREVER OS PRODUTOS COM ESTOQUE
+        if (arquivo == NULL) {
+            printf("Arquivo não existe."); // programação defensiva...
+            exit(1);
         }
-    }
-    if (!idExiste) {
-        printf("Produto com ID %d não encontrado.\n", id);
+
+        for (int i = 0; i <totalProdutos; i++ ) { // aqui sera um laço for pra tipo percorrer todos os structs de produtos e assim escreve-los na nova txt
+            fprintf(arquivo, "ID do produto: %d\n Produto: %s\n Preço por unidade: %.2f\n Desconto aplicado: %.2f\n Valor Final: %.2f\n Estoque: %d\n------------------------\n",produtos[i].id,
+                    produtos[i].nomeProduto,
+                    produtos[i].precoUnidade,
+                    produtos[i].desconto,
+                    produtos[i].valorFinal,
+                    produtos[i].qtd);
+
+        }
+        fclose(arquivo);
     }
 
-}
+    void adicionarItemEstoque() {
+        carregarProdutos();
+        bool idExiste = false; // boolean pra controlar o laço se o id existir no array struct.
+        int id, quantidade; // declarando o id e quantidade que do produto em questao...
+        printf("Insira o ID do produto: ");
+        scanf("%d", &id);
+        for (int i = 0; i <totalProdutos; i++) {
+            if (produtos[i].id == id) {
+                idExiste = true;
+                printf("Produto selecionado: %s\n", produtos[i].nomeProduto);
+                printf("Digite a quantidade de estoque do produto: " );
+                scanf("%d", &quantidade);
+                produtos[i].qtd += quantidade;
+                printf("Estoque atualizado com sucesso!\n Estoque atual do produto: %d", produtos[i].qtd);
+                break;
+            }
+        }
+        if (!idExiste) {
+            printf("Produto com ID %d não encontrado.\n", id);
+        }
+
+    }
