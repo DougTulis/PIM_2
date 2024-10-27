9e49ea2a4d2d714696bde6dd72e09a4152317cdc
Douglas Engratulis <dengratulis@gmail.com>
2024-10-27 10:47:59 -0300

    Demanda improcedente, teste ñ deu certo...

--- main.c
+++ main.c
@@ -29,60 +29,55 @@ void listarItens();
 int main() {
     int opcao;
     setlocale(LC_ALL, NULL);
-    printf("\n=== Menu de Estoque===\n"); // submenu do estoque...
-    printf("1. Adicionar Item\n");
-    printf("2. Remover Item\n");
-    printf("3. Listar Itens\n");
-    printf("0. Sair\n");
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
+    carregarProdutos();
+    do {
+        printf("\n=== Menu de Estoque===\n"); // submenu do estoque...
+        printf("1. Adicionar Item\n");
+        printf("2. Remover Item\n");
+        printf("3. Listar Itens\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
-    switch(opcao) {
-    case 1:
-        adicionarItemEstoque();
-        break;
-    case 2:
-        // removerItem(); (em manutenção..)
-        break;
-    case 3:
-        carregarProdutos();
-        break;
-    case 0:
-        printf("Saindo do programa...\n");
-        break;
-    default:
-        printf("Opção inválida! Tente novamente.\n");
-    }
+        switch(opcao) {
+        case 1:
+            adicionarItemEstoque();
+            break;
+        case 2:
+            // removerItem(); (em manutenção..)
+            break;
+        case 3:
+            listarItens();
+            break;
+        case 0:
+            printf("Saindo do programa...\n");
+            salvarProdutosVinculadoComEstoque();
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 0);
 
     return 0;
 }
 
 
-/* void listarItens() { /// leitura dos produtos existentes, retornando praticamente o struct criado pelo vinicius
-    FILE *arquivo;
-    arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\Produtos.txt", "r");
-    if (arquivo == NULL) {
-        printf("erro ao abrir o arquivo de estoque ou arquivo não existe.\n"); // Progrmaação defensiva
-        exit(0);
-    }
-
-
-    while(fscanf(arquivo, "%d,%99[^,],%d,%f,%f,%f", &prod.id, prod.nomeProduto, &prod.qtd, &prod.precoUnidade, &prod.desconto, &prod.valorFinal) == 6) {
-
-        printf("ID: %d\n", prod.id);
-        printf("Nome: %s\n", prod.nomeProduto);
-        printf("Quantidade atual do estoque: %d\n", prod.qtd);
-        printf("Preço Unidade: %.2f\n", prod.precoUnidade);
-        printf("Desconto: %.2f\n", prod.desconto);
-        printf("Valor Final: %.2f\n", prod.valorFinal);
+void listarItens() {
+      printf("\n=== Lista de Produtos ===\n");
+    for (int i = 0; i < totalProdutos; i++) {
+        printf("ID: %d\n", produtos[i].id);
+        printf("Nome: %s\n", produtos[i].nomeProduto);
+        printf("Preço Unidade: %.2f\n", produtos[i].precoUnidade);
+        printf("Desconto: %.2f\n", produtos[i].desconto);
+        printf("Valor Final: %.2f\n", produtos[i].valorFinal);
+        printf("Estoque: %d unidades\n", produtos[i].qtd);
         printf("------------------------\n");
     }
-    fclose(arquivo);
-}*/
+}
 
 void carregarProdutos() {
     totalProdutos = 0; // o total do array precisa começa com 0
-    FILE *arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\Produtos.txt", "r"); // acesso o arquivo
+    FILE *arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\Produtos.txt", "r"); // acesso o arquivo do vinicius
     if (arquivo == NULL) {
         printf("Arquivo não existe."); // programação defensiva...
         exit(1);
@@ -93,21 +88,21 @@ void carregarProdutos() {
                  &produtos[totalProdutos].desconto,
                  &produtos[totalProdutos].valorFinal) == 5) {
         printf("ID: %s\n", produtos[totalProdutos].nomeProduto);
-        produtos[totalProdutos].qtd = 0; // aqui vai começar com 0 pq vai ser definido dps;
+
         totalProdutos = totalProdutos + 1; // contador de produtos, e vai percorrer o array de produtos, assim ne precisa de for..
     }
     fclose(arquivo);
 }
 
 void salvarProdutosVinculadoComEstoque() {
-    FILE *arquivo = fopen("E:\Linguagem_C\Projeto PIM 2\Projeto\bin\Debug\ProdutoEstoque.txt", "w"); // PARA ESCRITA
+    FILE *arquivo = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\ProdutoEstoque.txt", "w"); // PASTA TXT NOVA QUE VOU ESCREVER OS PRODUTOS COM ESTOQUE
     if (arquivo == NULL) {
         printf("Arquivo não existe."); // programação defensiva...
         exit(1);
     }
 
     for (int i = 0; i <totalProdutos; i++ ) { // aqui sera um laço for pra tipo percorrer todos os structs de produtos e assim escreve-los na nova txt
-        sprintf("ID do produto: %d\n Produto: %s\n Preço por unidade: %.2f\n Desconto aplicado: %.2f\n Valor Final: %.2f",produtos[i].id,
+        fprintf(arquivo, "ID do produto: %d\n Produto: %s\n Preço por unidade: %.2f\n Desconto aplicado: %.2f\n Valor Final: %.2f\n Estoque: %d\n",produtos[i].id,
                 produtos[i].nomeProduto,
                 produtos[i].precoUnidade,
                 produtos[i].desconto,
@@ -127,11 +122,15 @@ void adicionarItemEstoque() {
         if (produtos[i].id == id) {
             idExiste = true;
             printf("Produto selecionado: %s\n", produtos[i].nomeProduto);
-            printf("Digite a quantidade de estoque do produto: %d", produtos[i].qtd);
+            printf("Digite a quantidade de estoque do produto: " );
+            scanf("%d", &quantidade);
+            produtos[i].qtd += quantidade;
+            printf("Estoque atualizado com sucesso!\n Estoque atual do produto: %d", produtos[i].qtd);
             break;
         }
     }
     if (!idExiste) {
         printf("Produto com ID %d não encontrado.\n", id);
-        }
+    }
+
 }
