a59c327929285213dbe725050d54273351a72949
Douglas Engratulis <dengratulis@gmail.com>
2024-11-10 21:09:57 -0300

    PROCEDENTE

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ void listarItens();
 
 int main() {
     int opcao;
-    setlocale(LC_ALL, NULL);
+
     carregarProdutos();
     do {
         printf("\n=== Menu de Estoque===\n"); // submenu do estoque...
@@ -117,23 +117,34 @@ void adicionarItemEstoque() {
 }
 
 void removerItemEstoque() {
-    int qtdEstoque, idEstoque, qtdRemover;
-    FILE *arquivoEstoque = fopen("E:\\Linguagem_C\\Projeto PIM 2\\Projeto\\bin\\Debug\\ProdutoEstoque.txt", "r"); //aqui eu acesso o arquivo estoque pra poder ler e retirar a qtd desejada.
-    while(fscanf(arquivoEstoque,"%*[^\n]\n%*[^\n]\n%*[^\n]\n%*[^\n]\nEstoque: &d", &qtdEstoque)); // condicao de leitura
-    if (arquivoEstoque != NULL) { // programação defensiva!
-        printf("Escreva o ID do item que você deseja remover do estoque: ");
-        scanf("%d", &idEstoque);
-        for (int i = 0; i < totalProdutos; i++) {
-            if (produtos[i].id == idEstoque) {
-                printf("Produto selecionado: %s\n", produtos[i].nomeProduto);
-                printf("Selecione a quantidade de itens que você deseja remover: ");
-                scanf("%d", &qtdRemover);
+    int idEstoque, qtdRemover;
+    bool idExiste = false;
+
+    printf("Escreva o ID do item que você deseja remover do estoque: ");
+    scanf("%d", &idEstoque);
+
+    for (int i = 0; i < totalProdutos; i++) {
+        if (produtos[i].id == idEstoque) {
+            idExiste = true;
+            printf("Produto selecionado: %s\n", produtos[i].nomeProduto);
+            printf("Selecione a quantidade de itens que você deseja remover: ");
+            scanf("%d", &qtdRemover);
+
+            if (produtos[i].qtd >= qtdRemover) {
                 produtos[i].qtd -= qtdRemover;
+                printf("Estoque atualizado com sucesso!\nEstoque atual do produto: %d\n", produtos[i].qtd);
+            } else {
+                printf("Erro: Quantidade insuficiente em estoque.\n");
             }
+
+            salvarProdutos();
+            break;
         }
-      salvarProdutos();
     }
-    fclose(arquivoEstoque);
+
+    if (!idExiste) {
+        printf("Produto com ID %d não encontrado.\n", idEstoque);
+    }
 }
 
 void salvarProdutos() {
